475b4ece491f5a44b86536dcd330bc4deb34e372
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2021-02-03 19:32:09 +0100

    Prepare stuff for Docker

---
+++ Práctica 3/build.sh
@@ -0,0 +1,2 @@
+cd Backend && sudo docker build -t prryplatypus/calcback:latest . && sudo docker push prryplatypus/calcback
+cd Frontend && sudo docker build -t prryplatypus/calcfront:latest . && sudo docker push prryplatypus/calcfront
