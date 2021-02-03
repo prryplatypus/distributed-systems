76eb7c840dcf6cf4ba9c107ec84d2ee4b4e479af
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2021-02-03 19:55:44 +0100

    Split build.sh in lines

--- Práctica 3/build.sh
+++ Práctica 3/build.sh
@@ -1,2 +1,6 @@
-cd Backend && sudo docker build -t prryplatypus/calcback:latest . && sudo docker push prryplatypus/calcback
-cd Frontend && sudo docker build -t prryplatypus/calcfront:latest . && sudo docker push prryplatypus/calcfront
+cd Backend \
+&& sudo docker build -t prryplatypus/calcback:latest . \
+&& sudo docker push prryplatypus/calcback \
+&& cd Frontend \
+&& sudo docker build -t prryplatypus/calcfront:latest . \
+&& sudo docker push prryplatypus/calcfront
