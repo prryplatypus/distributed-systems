94b126a8c721ceeb1e27be50370858ec73094713
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2021-02-03 20:12:36 +0100

    smh

--- Práctica 3/build.sh
+++ Práctica 3/build.sh
@@ -1,6 +1,6 @@
 cd Backend \
 && sudo docker build -t prryplatypus/calcback:latest . \
 && sudo docker push prryplatypus/calcback \
-&& cd Frontend \
+&& cd ../Frontend \
 && sudo docker build -t prryplatypus/calcfront:latest . \
 && sudo docker push prryplatypus/calcfront
